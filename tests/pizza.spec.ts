aefbfa7edbd3610da42787633e8dd560c81cb096
runn3rman <grantgardner455@gmail.com>
2026-02-10 23:02:44 -0700

    added playwright spec

---
+++ tests/pizza.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from 'playwright-test-coverage';
+
+test('home page', async ({ page }) => {
+  await page.goto('/');
+
+  expect(await page.title()).toBe('JWT Pizza');
+});
+
+test('purchase with login', async ({ page }) => {
+  
+});
