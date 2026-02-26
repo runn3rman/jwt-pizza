80ba3ce629f59cc12c20a272fc702e7c97907ec9
runn3rman <grantgardner455@gmail.com>
2026-02-25 22:52:42 -0700

    setUser

--- src/views/dinerDashboard.tsx
+++ src/views/dinerDashboard.tsx
@@ -9,6 +9,7 @@ import Button from '../components/button';
 
 interface Props {
   user: User | null;
+  setUser: (user: User) => void;
 }
 
 export default function DinerDashboard(props: Props) {
@@ -36,6 +37,15 @@ export default function DinerDashboard(props: Props) {
   }
 
   async function updateUser() {
+    let updatedUser: User = {
+      id: user.id,
+      name: nameRef.current?.value,
+      email: emailRef.current?.value,
+      password: passwordRef.current?.value || undefined,
+      roles: user.roles,
+    };
+
+    props.setUser(updatedUser);
     setTimeout(() => {
       HSOverlay.close(document.getElementById('hs-jwt-modal')!);
     }, 100);
